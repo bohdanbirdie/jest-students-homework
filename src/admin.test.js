c5b300649a6d9dee80b554fa89ed103ee809b44c
bohdanbirdie <voja4ok@yahoo.com>
2018-08-26 00:02:45 +0300

    add coverage

---
+++ src/admin.test.js
@@ -0,0 +1,6 @@
+const Admin = require('./admin');
+// const admin = new Admin();
+
+// test('Example test case', () => {
+//   expect(chat.logMessage('test')).toBe('Chat log: test');
+// })
---
+++ src/user.test.js
@@ -0,0 +1,6 @@
+const User = require('./user');
+// const user = new User();
+
+// test('Example test case', () => {
+//   expect(chat.logMessage('test')).toBe('Chat log: test');
+// })
